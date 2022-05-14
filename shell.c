01d613605efb93d443105bbc2c6557ce67edde64
odilon-cloud <oirambna@gmail.com>
2022-05-14 17:49:58 +0000

    commit

---
+++ shell.c
@@ -0,0 +1,49 @@
+#include "simple_shell.h"
+
+/**
+ * main - main function.
+ *
+ * Return: 0.
+ */
+int main(void)
+{
+	int name_cond = 1;
+	int sign = 1;
+	char *line = NULL;
+	char **tokens = NULL;
+
+	signal(SIGINT, sigint_handler);
+
+	while (name_cond)
+	{
+		write(STDIN_FILENO, "$ ", 2);
+
+		line = reading();
+		sign = semicolon(line);
+		tokens = cutting(line);
+		name_cond = comparing(tokens);
+
+		if (name_cond == 1 && sign == 1)
+		{
+			name_cond = executing(tokens, line);
+		}
+		free(line);
+		free(tokens);
+	}
+	return (EXIT_SUCCESS);
+}
+
+/**
+ * sigint_handler -  Signal Handler function.
+ * @sigint: Reset handler to catch SIGINT next time.
+ *
+ * Return: Nothing.
+ */
+void sigint_handler(int sigint)
+{
+	(void)sigint;
+
+	signal(SIGINT, sigint_handler);
+	write(STDOUT_FILENO, "\n$ ", 3);
+	fflush(stdout);
+}
---
+++ simpleshell.h
@@ -0,0 +1,47 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+/* Libraries */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+/* Main Prototypes */
+int main(void);
+
+char *reading(void);
+
+char **cutting(char *line);
+
+int executing(char **tokens, char *line);
+
+int comparing(char **tokens);
+
+void sigint_handler(int sigint);
+
+int semicolon(char *line);
+
+/* Built-in Prototypes */
+void _env(char **commands);
+
+int _cd(char **args);
+
+/* Errors Prototypes */
+void errors(char __attribute__((unused)) **tokens);
+
+/* Auxiliar Prototypes */
+int _putchar(char c);
+
+int _strlen(char *s);
+
+int _strcmp(char *s1, char *s2);
+
+char *_strcat(char *dest, char *src);
+
+char *_strcpy(char *dest, char *src);
+
+extern char **environ;
+#endif
